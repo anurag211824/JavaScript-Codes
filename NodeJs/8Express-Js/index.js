cc2751ef20ea89f5caca513633990373514e419a
Anurag Kumar <138526681+anurag211824@users.noreply.github.com>
2024-07-27 02:02:30 +0530

    study express js

--- NodeJs/8Express-Js/index.js
+++ NodeJs/8Express-Js/index.js
@@ -1,2 +1,27 @@
-const http=require("http")
-const fs=require("fs")
+const http = require("http");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.send("hello from home page");
+});
+
+app.get("/about", (req, res) => {
+  return res.send(
+    "hello from About page" +
+      "hey " +
+      req.query.name +
+      " you are " +
+      req.query.age
+  );
+});
+const myserver = http.createServer(app);
+
+// myserver.listen(8080, () => {
+//   console.log(`server started at port 8080`);
+// });
+
+app.listen(8080, () => {
+  console.log(`server started at port 8080`);
+});
