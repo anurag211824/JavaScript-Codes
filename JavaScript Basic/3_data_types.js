2899c83362155590cc800df17ef5cfffc1b4f7c0
Anurag Kumar <138526681+anurag211824@users.noreply.github.com>
2024-10-18 00:47:22 +0530

    Update 3_data_types.js

--- JavaScript Basic/3_data_types.js
+++ JavaScript Basic/3_data_types.js
@@ -10,7 +10,75 @@ console.log(number);
 console.log(message);
 console.log(isActive);
 console.log(numbers);
-console.log(person);
+console.log(person)
+
+
+
+
+//bidint
+// Regular Number (can lose precision for very large numbers)
+let regularNumber = 9007199254740991; // Largest safe integer in JavaScript
+console.log(regularNumber + 1); // 9007199254740992
+console.log(regularNumber + 2); // 9007199254740992 (incorrect due to precision loss)
+
+// BigInt (can handle arbitrarily large integers)
+let bigIntNumber = 9007199254740991n; // Adding 'n' makes it a BigInt
+console.log(bigIntNumber + 1n); // 9007199254740992n
+console.log(bigIntNumber + 1n); // 9007199254740993n
+
+
+//symbols
+// Creating symbols
+const symbol1 = Symbol('description1');
+const symbol2 = Symbol('description2');
+
+// Symbols are unique
+console.log(symbol1 === symbol2); // false
+
+// Using symbols as object keys
+let obj = {
+  [symbol1]: 'Value for symbol1',
+  [symbol2]: 'Value for symbol2'
+};
+
+console.log(obj[symbol1]); // Value for symbol1
+console.log(obj[symbol2]); // Value for symbol2
+
+//date dataType
+
+// Creating a new Date object with the current date and time
+let currentDate = new Date();
+console.log(currentDate); // Prints current date and time
+
+// Creating a specific date (January 1, 2024)
+let specificDate = new Date('2024-01-01');
+console.log(specificDate); // Mon Jan 01 2024 00:00:00
+
+// Getting parts of the date
+console.log(currentDate.getFullYear()); // Get the year
+console.log(currentDate.getMonth()); // Get the month (0-indexed, 0 = January)
+console.log(currentDate.getDate()); // Get the day of the month
+
+// Modifying dates
+let modifiedDate = new Date();
+modifiedDate.setFullYear(2025); // Change the year to 2025
+console.log(modifiedDate);
+
+//array
+let array=[1,2,3,4,5,5,5]
+console.log(array);
+
+let myNumber="9334759512"
+console.log(parseInt(myNumber));
+
+let str1="100"
+console.log(str1-10);
+console.log(str1+10);
+
+
+
+
+
 
 //number
 //bigint
@@ -21,4 +89,4 @@ console.log(person);
 //symbol+>unique
 
 //object
-console.log(typeof number);
+//console.log(typeof number);
