9db572b22eeb7e24eea7cab08b2b50b12035c572
Anurag Kumar <138526681+anurag211824@users.noreply.github.com>
2025-01-22 02:06:33 +0530

    dataTypes and variables

---
+++ IntroductionToJavaScript/2DataTypes.js
@@ -0,0 +1,29 @@
+let a=3
+//how to conert a string into a number
+let b=+'1000'
+console.log(a+b);
+
+let d=parseInt('100daiaafh')
+console.log(d);
+
+let s='321231';
+console.log(typeof(s+''));
+
+
+let x = 1234567890123456789012345n;
+console.log(x);
+
+
+let p = 9007199254740995n;
+let q = 9007199254740995n;
+let r = p * q;
+
+console.log(r);
+
+
+let m = Number.MAX_SAFE_INTEGER;
+console.log(m);
+let n=Number.MIN_SAFE_INTEGER;
+console.log(n);
+
+
