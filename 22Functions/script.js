f7902e6eb11cb342572df57d89011c25ebccb817
Anurag Kumar <138526681+anurag211824@users.noreply.github.com>
2025-02-10 20:44:02 +0530

    Execution context

---
+++ 22Functions/script.js
@@ -0,0 +1,15 @@
+function introduceMe(username,profession){ 
+    console.log('Hi');
+    console.log(`Hi my name is ${username} and my profile is ${profession}`);
+    console.log('Nice to meet you');
+   
+    
+}
+introduceMe('John','Software Engineer');
+introduceMe('Jane','Web Developer');
+introduceMe('Jim','Mechnaical Engineer');
+introduceMe();
+
+
+
+
---
+++ 23ExecutionContext/script.js
@@ -0,0 +1,27 @@
+debugger
+sayHi();
+const userName = "Anurag Kumar";
+const userAge = 21;
+
+function sayHi() {
+    const a=14
+    const b=12
+    add(a,b)
+}
+function add(a,b){
+    const x=7
+    const y=8
+    console.log(a+b+x+y);
+    return x+y;
+}
+console.log("Done");
+
+// console.log("Start");
+// let x = 5;
+// factorial(x);
+// function factorial(x){
+//     if(x==0 || x==1)
+//         return 1;
+//     return factorial(x-1)*x;
+// }
+// console.log("Done");
